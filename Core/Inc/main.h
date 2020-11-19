4793b6e1091c70f098ca7a72a0f2657c809ac6c9
Mike <photo.mickey@gmail.com>
2020-11-19 13:34:19 +0300

    Solid LED on bootloader

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,8 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define BOOT1_Pin GPIO_PIN_13
+#define BOOT1_Pin       GPIO_PIN_13
 #define BOOT1_GPIO_Port GPIOC
+#define LED_Pin         GPIO_PIN_7
+#define LED_GPIO_Port   GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -65,9 +65,9 @@
 
 #define USBD_VID                     1155
 #define USBD_LANGID_STRING           1033                              /* US English code */
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS                  57113 //57105
-#define USBD_PRODUCT_STRING_FS       "STM32 DownLoad Firmware Update"
+#define USBD_MANUFACTURER_STRING     "Energan"
+#define USBD_PID_FS                  57105
+#define USBD_PRODUCT_STRING_FS       "Energan DownLoad Firmware Update"
 #define USBD_CONFIGURATION_STRING_FS "DFU Config"
 #define USBD_INTERFACE_STRING_FS     "DFU Interface"
 
